de8ac25a3fd4f186b0fc00421cd3682b5522b1db
Richard Osungu <126188632+Osungu@users.noreply.github.com>
2026-09-18 08:24:30 +0300

    Use local portfolio runtime dependencies

---
+++ assets/js/resources-15ef50f1.js
@@ -0,0 +1,4 @@
+window.__resources = {
+  "https://unpkg.com/react@18.3.1/umd/react.production.min.js": "assets/vendor/react-d949f1c3.js",
+  "https://unpkg.com/react-dom@18.3.1/umd/react-dom.production.min.js": "assets/vendor/react-dom-35f4f974.js"
+};
